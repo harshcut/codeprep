82bba89b50092f3a04d9fccbfac5923ba351307f
harshcut <harshskarande@gmail.com>
2023-02-05 00:23:37 +0530

    setup client workspace with nextjs

---
+++ client/pages/_app.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Code Prep</title>
+        <meta name="description" content="Collaborate and Conquer Coding Challenges" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
