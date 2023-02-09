fb438c2d6551ed6d50e0a47c48c781bb33ec4b1e
harshcut <harshskarande@gmail.com>
2023-02-09 14:40:11 +0530

    setup ui workspace and link to client

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,12 @@ const { fontFamily } = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} \*/
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: ['class', '[data-theme="dark"]'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    '../ui/src/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {
       fontFamily: {
