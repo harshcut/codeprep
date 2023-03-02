345321646beaa279d607883a3fef7715cc908f89
harshcut <harshskarande@gmail.com>
2023-03-02 19:57:22 +0530

    create top navigation layout

--- client/app/me/layout.tsx
+++ client/app/me/layout.tsx
@@ -1,3 +1,10 @@
+import Navbar from './navbar'
+
 export default async function MeLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>
+  return (
+    <div className="w-full max-w-6xl mx-auto">
+      <Navbar />
+      {children}
+    </div>
+  )
 }
---
+++ client/app/me/navbar.tsx
@@ -0,0 +1,58 @@
+'use client'
+
+import Image from 'next/image'
+import { useSession, signOut } from 'next-auth/react'
+import { LogOut, Settings, User, Plus, UserPlus } from 'lucide-react'
+import { Avatar, DropdownMenu } from 'ui'
+
+export default function Navbar() {
+  const { data: session } = useSession()
+  const email = session?.user?.email
+
+  return (
+    <nav className="h-16 px-6 flex items-center border-b border-x border-slate-200">
+      <Image src="/head.png" width={100} height={25} alt="Code Prep" draggable="false" />
+      <DropdownMenu>
+        <DropdownMenu.Trigger asChild>
+          <Avatar className="ml-auto cursor-pointer">
+            <Avatar.Image
+              src={`https://source.boringavatars.com/bauhaus/120/${email}?colors=DACDAC,F39708,F85741,0E9094,1E1801`}
+              draggable={false}
+            />
+            <Avatar.Fallback>{email?.slice(0, 2).toUpperCase()}</Avatar.Fallback>
+          </Avatar>
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content className="w-56">
+          <DropdownMenu.Label>My Account</DropdownMenu.Label>
+          <DropdownMenu.Separator />
+          <DropdownMenu.Group>
+            <DropdownMenu.Item disabled>
+              <User className="mr-2 h-4 w-4" />
+              <span>Profile</span>
+            </DropdownMenu.Item>
+            <DropdownMenu.Item disabled>
+              <Settings className="mr-2 h-4 w-4" />
+              <span>Settings</span>
+            </DropdownMenu.Item>
+          </DropdownMenu.Group>
+          <DropdownMenu.Separator />
+          <DropdownMenu.Group>
+            <DropdownMenu.Item disabled>
+              <Plus className="mr-2 h-4 w-4" />
+              <span>Playground</span>
+            </DropdownMenu.Item>
+            <DropdownMenu.Item disabled>
+              <UserPlus className="mr-2 h-4 w-4" />
+              <span>Invite users</span>
+            </DropdownMenu.Item>
+          </DropdownMenu.Group>
+          <DropdownMenu.Separator />
+          <DropdownMenu.Item onClick={() => signOut()}>
+            <LogOut className="mr-2 h-4 w-4" />
+            <span>Log out</span>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu>
+    </nav>
+  )
+}
